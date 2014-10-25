b72a8ca6568e6a85f374287e0b329f1d1387dba3
=carolinux <=karolina.alexiou@teralytics.ch>
2014-10-25 20:47:17 +0200

    Using scikit image for simpler image representation and access to image algorithms

--- pic.py
+++ pic.py
@@ -1,57 +1,53 @@
-from PIL import Image
+import skimage.io as io
 import math
 import numpy as np
 import copy
+from matplotlib import pyplot as plt
 
 
 def checkerboard(img, parts):
         """Replace all image parts with black and white (just for fun)"""
-        pix = img.load()
         for i,part in enumerate(parts):
                 pixels = part.get_all_pixels()
                 for px in pixels:
-                        pix[px] = (0,0,0) if i%2==0 else (255,255,255)
+                        img[px] = (0,0,0) if i%2==0 else (255,255,255)
 
 def tile_one_part(img, parts):
         """Get a random part to tile all over the image"""
         #FIXME what happens if part is smaller? (idx=14) not robust now
-        pix = img.load()
         for i,part in enumerate(parts):
                 pixels = part.get_all_pixels()
                 origin = part.get_origin()
 
                 for px in pixels:
                         c = parts[16].get_color(*px, origin=origin)
-                        pix[px]=c
+                        img[px]=c
+
+def get_parts(img, numXtiles, numYtiles):
+        """Divide the image equally int X*Y parts"""
+        width_x = int(math.ceil(img.shape[0]*1.0/numXtiles))
+        width_y = int(math.ceil(img.shape[1]*1.0/numYtiles))
+        parts = []
+        for i in range(0,img.shape[0],width_x):
+                for j in range(0,img.shape[1],width_y):
+                        parts.append(ImagePart.from_image(img,width_x, width_y, origin=(i,j)))
+                        
+        return parts
+
 
 
 def get_image_matrix(img):
-        """
-        Retun a 2D array of the colors of every pixel
-        img (PIL.Image): image
-        """
-        #im = Image.open(filename) #Can be many different formats.
-        #img = img.resize((basewidth,hsize), PIL.Image.ANTIALIAS)
-        pix = img.load()
-
-        matrix = []
-
-        for w in range(img.size[0]):
-                matrix.append([])
-                for h in range(img.size[1]):
-                        matrix[w].append(pix[w,h])
-        return np.array(matrix)
+       return img
 
 
 class ImagePart(object):
         """A class that represents part of an image"""
 
-        matrix = None # the np.arr of teh pixels
+        matrix = None # the np.arr of teh  pixels
         ll = None # lower left point - origin
         w = None
         y = None
         def __init__(self, matrix, ll):
-
                 self.w =len(matrix)
                 self.y=len(matrix[0])
                 self.ll =ll
@@ -86,7 +82,7 @@ class ImagePart(object):
                 return self.ll
 
         def get_all_pixels(self):
-                """Return all the pixel coordinates (as x,y tuples) of the original image that this part covers"""
+                """Return all the  pixel coordinates (as x,y tuples) of the original image that this part covers"""
 
                 return [(self.ll[0]+x,self.ll[1]+y) for (x,y,_),_ in np.ndenumerate(self.matrix)] # (x,y,_),_ wtf lawl
 
@@ -97,6 +93,7 @@ class ImagePart(object):
 
         def match_score(self, other_part):
                 """ See if a picture matches the dominant colors etc of this particular part nicely"""
+                # resize pictures to have same size (resize method?)
                 #blur both and have diff thresholds for lightness and hue,sat? lightness may be importanter for contrast...?
                 # maybe a tile should have a contrast score too
                 pass
@@ -105,30 +102,13 @@ class ImagePart(object):
                 pass
 
         
-        
-filename="./kitteh.jpg"
-im = Image.open(filename)
-num_x=10
-num_y=5
-
-
-
-def get_parts(img, numXtiles, numYtiles):
-        width_x = int(math.ceil(img.size[0]*1.0/numXtiles))
-        width_y = int(math.ceil(img.size[1]*1.0/numYtiles))
-        parts = []
-
-        for i in range(0,img.size[0],width_x):
-                for j in range(0,img.size[1],width_y):
-                        parts.append(ImagePart.from_image(img,width_x, width_y, origin=(i,j)))
-                        
-
-        return parts
-
-
-parts = get_parts(im, num_x, num_y)
-checkerboard(im,parts)
-#tile_one_part(im,parts)
-
-
-im.show()
+if __name__=="__main__":        
+        filename="./kitteh.jpg"
+        im = io.imread(filename)
+        num_x=10
+        num_y=5
+        parts = get_parts(im, num_x, num_y)
+        checkerboard(im,parts)
+        #tile_one_part(im,parts)
+        plt.imshow(im) 
+        plt.show()
